2e58b681001e1426956f48000293e9da05a5d7a1
Aditya Singh Chandel <adityakr1466@hotmail.com>
2024-08-04 17:04:43 +0530

    Updating the code for vercel

--- foodie/settings.py
+++ foodie/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-_n38i$tq#_srp*$o^rj^@-1091a*f%54cq-u-9%36abdfoz!c(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -109,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -120,12 +120,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'foodie/static/')
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'foodie/static/')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'foodie/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
